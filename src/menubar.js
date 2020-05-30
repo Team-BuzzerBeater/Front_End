b455b0df7edef443512b1d8206c531975ca59b92
lang8 <qwerty67852@gmail.com>
2020-05-30 15:52:24 +0900

    create sidebar

---
+++ src/menubar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const menubar = () => (
+    <ul className='gnb'>
+        <li>
+            <a href='#none' className='tab day on'>일요일</a>
+        </li>
+        <li>
+            <a href='#none' className='tab day'>토요일</a>
+        </li>
+    </ul>
+)
+
+export default menubar;
