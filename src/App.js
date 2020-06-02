a0ab2a6b59bdab206b66241e9db51850daea4ce4
lang8 <qwerty67852@gmail.com>
2020-06-02 23:37:17 +0900

    create grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 
+function drawCircle(){
+
+}
+
 function App() {
   return (
     <div className="app">
@@ -11,16 +15,22 @@ function App() {
         </ul>
       </div>
 
-      <div className="dropdwon">
-        <select name="Team">
-          <option value='1'>전북 현대 모터스</option>
-          <option value='2'>울산 현대 호랑이</option>
-        </select>
-        
-        <select name="Player">
-          <option value='1'>멋장이 김민준</option>
-          <option value='2'>미래의 주커버그</option>
-        </select>
+      <div className='contentblock'>
+        <div className="dropdown">
+          <select name="Team">
+            <option value= '0' selected>Please select team</option>
+            <option value='1'>전북 현대 모터스</option>
+            <option value='2'>울산 현대 호랑이</option>
+          </select>
+          
+          <select name="Player">
+            <option value= '0' selected>Please select player</option>
+            <option value='1'>멋장이 김민준</option>
+            <option value='2'>미래의 주커버그</option>
+          </select>
+        </div>
+        <div className="sketch"></div>
+        <div className="textbox"></div>
       </div>
     </div>
   );
